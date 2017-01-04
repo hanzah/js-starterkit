85467de9576c541210eae3030754082594160389
Hani <Hani@Hanis-MacBook-Air.local>
2017-01-04 15:27:08 +0100

    Changing folder naming

--- src/views/SimCardsView/SimCardsView.js
+++ src/views/SimCardsView/SimCardsView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { DesktopMediaQuery, TabletMediaQuery, MobileMediaQuery } from 'utils/CustomMediaQuery'
-import {  DesktopWelcomeComponent, TabletWelcomeComponent, MobileWelcomeComponent } from '../../components/Welcome'
+import {  DesktopWelcomeComponent, TabletWelcomeComponent, MobileWelcomeComponent } from 'components/welcome'
 
 export default class Welcome extends React.Component {
   render() {
