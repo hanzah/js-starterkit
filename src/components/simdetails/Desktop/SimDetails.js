a4c73ac1df7d791bb74ce22a2d7b2c8d572cf6de
Fex Core <fedjase@maestralsolutions.com>
2017-01-19 10:58:27 +0100

    added pending status div

--- src/components/simdetails/Desktop/SimDetails.js
+++ src/components/simdetails/Desktop/SimDetails.js
@@ -1,32 +1,48 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import SimDetailsHeader from './simdetailsheader';
 import SimStatusDiagram from './simstatusdiagram';
 import SimDetailsInfo from './simdetailsinfo';
 import ChangeStatus from './changestatus';
 import Tabs from './tabs';
+import CurrentContext from 'utils/currentContext'
+import ImageSelector from 'utils/ImageSelector'
+
+const pending = ImageSelector(CurrentContext.theme, 'pending.png');
 
 import styles from './SimDetails.scss';
 
-const { object } = PropTypes
+const {object} = PropTypes
 
-const SimDetails = ({ sim }) => {
-	return(
+const SimDetails = ({sim}) => {
+  return (
     <div>
-      <SimDetailsHeader sim={sim} />
-			<div>
+      <SimDetailsHeader sim={sim}/>
+      <div>
+        <div className={styles.pending_changes}>
+          <div className={styles.pending_img}>
+            <img src={pending} alt="pending"/>
+          </div>
+          <div className={styles.pending_text}>
+            <div>
+              <span>There are some pending changes for this SIM, initiated by John Smith,
+                starting on 14.2.2017.</span>
+              <span>See History below for details.</span>
+            </div>
+          </div>
+        </div>
         <div className={styles.main_wrap}>
-          <ChangeStatus />
-          <div className={styles.sim_status_diagram}><SimStatusDiagram /></div>
-          <div className={styles.sim_details_info}><SimDetailsInfo /></div>
+          <ChangeStatus/>
+          <div className={styles.sim_status_diagram}><SimStatusDiagram/></div>
+          <div className={styles.sim_details_info}><SimDetailsInfo/></div>
         </div>
       </div>
-      <Tabs />
+      <Tabs/>
     </div>
   );
 }
 
 SimDetails.propTypes = {
-	sim: object
+  sim: object
 }
 
 export default SimDetails;
